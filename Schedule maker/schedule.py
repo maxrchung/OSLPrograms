df72ff30b7296cd0695d5036769c2e65d04ab84e
waxchugdagwad <maxrc@uci.edu>
2015-07-03 18:41:40 -0700

    Commented schedule.py

--- Schedule maker/schedule.py
+++ Schedule maker/schedule.py
@@ -2,11 +2,11 @@ import urllib2
 from bs4 import BeautifulSoup
 import random
 
+# Input: the round section of the HTML
 file = open('test.txt', 'r')
 soup = BeautifulSoup(file)
 
-# print soup.prettify()
-
+# Finds all the match letters on the HTML page
 matchLetters = []
 for anchor in soup.find_all('a'):
     if 'class' in anchor.attrs:
@@ -14,7 +14,8 @@ for anchor in soup.find_all('a'):
             matchLetters.append(anchor.get_text().strip())
 
 players = []
-            
+
+# Finds all the players
 for div in soup.find_all('div'):
     if 'class' in div.attrs:
         if 'participant-present' in div['class']:
@@ -22,24 +23,27 @@ for div in soup.find_all('div'):
 
 matches = []
 
+# Makes a collection of (matchLetter, (player1, player2))
 for index in range(len(matchLetters)):
     player1 = index * 2
     player2 = index * 2 + 1
     matches.append((matchLetters[index], (players[player1], players[player2])))
 
+# Randomizes the matches
 randomMatches = []
 while len(matches) > 0:
     randomMatches.append(matches.pop(random.randint(0, len(matches) - 1)))
 
 output = open('output.html', 'w')
 
+# Function for writing line (wl) and putting a '\n' at the end
 def wl(file, string):
     file.write(string + "\n")
 
 wl(output, '<table>')
-    
+
+# Prints out to HTML based on data before
 starttime = 7.5 * 60
-    
 for index in range(len(randomMatches)):
     if index % 4 == 0:
         starttime = starttime + 30
