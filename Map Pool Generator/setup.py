b06c55338e630e44894d9d776059b31590d33ded
waxchugdagwad <maxrc@uci.edu>
2015-07-11 16:19:44 -0700

    Entered values for setup.py properly

--- Map Pool Generator/setup.py
+++ Map Pool Generator/setup.py
@@ -1,3 +1,3 @@
 from cx_Freeze import setup, Executable
 
-setup( name = "" , version = "0.1" , description = "" , executables = [Executable("")] , )
+setup( name = "" , version = "0.1" , description = "" , executables = [Executable(script = "MapPoolInterface.py")] , base="Win32GUI" )
