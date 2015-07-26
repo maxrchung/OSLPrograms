d8ee8e8223ba022e980c8b45f5a358004a144eed
VicSto <vstolle@uci.edu>
2015-07-25 20:13:46 -0700

    Final Final Build

--- Map Pool Generator/MapTocsv.py
+++ Map Pool Generator/MapTocsv.py
@@ -9,10 +9,11 @@ class CSVGenerator:
         self.maps = maps
         self.path = path
         self.sizes = sizes # sizes are [len(DT), len(HR), len(HD), len(NM), len(TB)
-            
+        self.counter = 0
     
     def write(self):
-        #csv write 
+        #csv write
+        
         with open(self.path, "wb") as csv_file:
             self.writer = csv.writer(csv_file, delimiter=',')
             self.writer.writerow(['Artist', 'Title', 'Difficulty', 'Link', 'Mod', 'Length', 'Star', 'Song No.'])
@@ -20,31 +21,34 @@ class CSVGenerator:
                 for line in self.maps:
                     self.writer.writerow(line)
             else:
+                
                 for i in range(self.sizes[0]):
-                    self.line = self.maps[i] #file line
+                    self.line = self.maps[self.counter]#file line
                     self.line[-1] = i
                     self.writer.writerow(self.line)
-
+                    self.counter +=1
+        
 
                 for i in range(self.sizes[1]):
-                    self.line = self.maps[i]
+                    self.line = self.maps[self.counter]
                     self.line[-1] = i
                     self.writer.writerow(self.line)
-
+                    self.counter +=1
+                    
                 for i in range(self.sizes[2]):
-                    self.line = self.maps[i] #file line
+                    self.line = self.maps[self.counter] #file line
                     self.line[-1] = i
                     self.writer.writerow(self.line)
-
+                    self.counter +=1
 
                 for i in range(self.sizes[3]):
-                    self.line = self.maps[i] #file line
+                    self.line = self.maps[self.counter] #file line
                     self.line[-1] = i
                     self.writer.writerow(self.line)
-
+                    self.counter +=1
 
                 for i in range(self.sizes[4]):
-                    self.line = self.maps[i] #file line
+                    self.line = self.maps[self.counter] #file line
                     self.line[-1] = i
                     self.writer.writerow(self.line)
-
+                    self.counter +=1
